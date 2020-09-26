fbce876d282f1d5f3487fd9bd31f3986d2e699a8
710429 <710429@SAIT224192IT.ACDM.DS.SAIT.CA>
2020-09-25 21:28:12 -0600

    Added validation added validation to form to ensure user typed in both last and first name

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -28,9 +28,20 @@ public class HelloWorldServlet extends HttpServlet {
         /*Gets the parameter from the form*/
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
+        
         /*sets the parameters and set as attributes so its available in jsp*/
         request.setAttribute("firstname", firstname);
         request.setAttribute("lastname", lastname);
+        
+        /*Adding validation, always checks for null FIRST in if statement*/
+        if(firstname == null || firstname.equals("") || (lastname == null) || lastname.equals(""))
+        {
+           getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
+                .forward(request, response);
+           //Dont let the rest of it run
+           return;
+        }
+        
         /*forwards to sayHello.jsp*/
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
                 .forward(request, response);
